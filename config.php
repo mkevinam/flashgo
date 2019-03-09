b83e06f2fcebbd097cfddc7ac201c1806093e4a9
mkevinam <37040656+mkevinam@users.noreply.github.com>
2019-03-09 07:53:32 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="kumpulanremaja";
+
+//user_gaid wajib isi
+$user_gaid= "fff945bd-c895-4b64-8358-de2f57002527";
+//user_token wajib di isi //
+$user_token="AbyLzDu50ls4jdnR";
+
+//user_id wajib di isi //
+$user_id="Facebook_2394979287455122";
+
+// invite_code wajib di isi //
+$invite_code="FAy4x6vt22";
+
+// Bearer wajib di isi //
+$Bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18yMzk0OTc5Mjg3NDU1MTIyIiwiZXhwIjoiMTU1NDY1MTIzNzc3MSJ9.fyrGZ-6QVn0xdB2USPhPPi6t-ajZCFDARQoDJGhbI0k";
+
+
+
+?>
